153a4f775a328731ed9d865d10b0923406e46580
ali5669 <1546936152@qq.com>
2025-05-28 20:26:04 +0800

    init backend

---
+++ final-chapters-backend/content-service/src/test/java/org/ali5669/contentservice/ContentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ali5669.contentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ final-chapters-backend/user-service/src/test/java/org/ali5669/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ali5669.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
