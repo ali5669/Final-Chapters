e9a70a0d3db219e99df08f5ae265c72de29ee81d
ali5669 <1546936152@qq.com>
2025-06-06 12:18:00 +0800

    feat: gateway 新增用户登录鉴权功能

---
+++ final-chapters-backend/gateway-service/src/main/java/org/ali5669/gatewayservice/config/AuthProperties.java
@@ -0,0 +1,16 @@
+package org.ali5669.gatewayservice.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "fc.auth")
+public class AuthProperties {
+    private List<String> includePaths;
+    private List<String> excludePaths;
+}
+
